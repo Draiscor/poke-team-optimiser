aba35b13b8dbf12ca071cbdbe07dd96ae9c9596e
Nathan Steer <nsteer@gmail.com>
2023-08-16 09:57:03 +0100

    [main] Removed the router

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { RouterProvider, createHashRouter } from "react-router-dom";
 
 const client = new ApolloClient({
 	uri: "https://beta.pokeapi.co/graphql/v1beta",
 	cache: new InMemoryCache(),
 });
 
-const router = createHashRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-]);
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ApolloProvider client={client}>
-			<RouterProvider router={router} />
+			<App />
 		</ApolloProvider>
 	</React.StrictMode>
 );

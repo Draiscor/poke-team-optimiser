5369df005383ca453c27005d4a9637ac0801acad
Nathan Steer <nsteer@gmail.com>
2023-08-16 09:28:34 +0100

    [main] Added router to try and fix gh pages issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
 const client = new ApolloClient({
 	uri: "https://beta.pokeapi.co/graphql/v1beta",
-	cache: new InMemoryCache()
+	cache: new InMemoryCache(),
 });
+
+const router = createHashRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ApolloProvider client={client}>
-			<App />
+			<RouterProvider router={router} />
 		</ApolloProvider>
 	</React.StrictMode>
 );
